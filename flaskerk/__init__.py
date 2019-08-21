697c83f9c6af87c8bfb6ad137f89bfe8adae4d02
keming <kemingy94@gmail.com>
2019-08-21 19:44:18 +0800

    add expt

--- flaskerk/__init__.py
+++ flaskerk/__init__.py
@@ -1,3 +1,4 @@
 from flaskerk.base import Flaskerk  # noqa: F401
+from flaskerk.exception import HTTPException  # noqa: F401
 
 __version__ = '0.1.0'
--- flaskerk/exception.py
+++ flaskerk/exception.py
@@ -3,9 +3,29 @@ from pydantic import BaseModel, Schema
 
 class HTTPValidationError(BaseModel):
     code: int = Schema(
+        422,
+        gt=100,
+        lt=600,
+        description='HTTP response status code',
+    )
+    description: str = 'Unprocessable Entity'
+
+
+class HTTPException(BaseModel):
+    code: int = Schema(
         ...,
         gt=100,
         lt=600,
         description='HTTP response status code',
     )
-    description: str
+    msg: str
+
+    class Config:
+        schema_extra = {
+            'examples': [
+                {
+                    'code': 422,
+                    'msg': 'Unprocessable Entity',
+                }
+            ]
+        }
