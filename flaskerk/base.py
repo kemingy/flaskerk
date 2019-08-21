01ca7dcdaed3f35be9c467ebff93e86378cf35a2
keming <kemingy94@gmail.com>
2019-08-21 19:44:18 +0800

    add expt

--- flaskerk/base.py
+++ flaskerk/base.py
@@ -4,18 +4,20 @@ from flask import Blueprint, abort, request, jsonify
 
 from flaskerk.config import default_config
 from flaskerk.view import APIview
-from flaskerk.exception import HTTPValidationError
+from flaskerk.exception import HTTPValidationError, HTTPException
 
 
 class Flaskerk:
     """
     :param app: Flask app instance
-
+    :param configs: key-value pairs in :class:`flaskerk.config.Config`
 
     """
-    def __init__(self, app, config=None):
+    def __init__(self, app, **configs):
         self.models = {}
-        self.config = config or default_config
+        self.config = default_config
+        for key, value in configs.items():
+            setattr(self.config, key, value)
 
         if app:
             self.init_app(app)
@@ -24,6 +26,7 @@ class Flaskerk:
         self.app = app
         self.update_config()
         self.models[HTTPValidationError.__name__] = HTTPValidationError.schema()
+        self.models[HTTPException.__name__] = HTTPException.schema()
         self.register()
         self.parse_path()
         app.openapi = self
@@ -84,12 +87,12 @@ class Flaskerk:
                 if method in ['HEAD', 'OPTIONS']:
                     continue
 
-                has_schema = hasattr(func, 'query') or hasattr(func, 'resp')
                 spec = {
                     'summary': func.__name__.capitalize(),
                     'operationID': func.__name__ + '__' + method.lower(),
                 }
-                if has_schema:
+
+                if hasattr(func, 'query'):
                     spec['requestBody'] = {
                         'content': {
                             'application/json': {
@@ -99,6 +102,8 @@ class Flaskerk:
                             }
                         }
                     }
+
+                if hasattr(func, 'resp'):
                     spec['responses'] = {
                         '200': {
                             'description': 'Successful Response',
@@ -133,6 +138,17 @@ class Flaskerk:
                         }
                     }
 
+                if hasattr(func, 'expt'):
+                    for code, msg in func.expt.items():
+                        spec['responses'][str(code)] = {
+                            'description': msg,
+                            'content': {
+                                'application/json': {
+                                    'schema': {}
+                                }
+                            }
+                        }
+
                 routes[str(rule)][method.lower()] = spec
 
         data = {
@@ -152,7 +168,7 @@ class Flaskerk:
         }
         self.config.data = data
 
-    def validate(self, query, resp):
+    def validate(self, query=None, resp=None, expt=[]):
         """
         validate JSON data according to Model schema
 
@@ -162,14 +178,14 @@ class Flaskerk:
         def decorate_validate_request(func):
             @wraps(func)
             def validate_request(*args, **kwargs):
-                if not issubclass(query, BaseModel):
+                if query and not issubclass(query, BaseModel):
                     abort(422, 'Unsupported request data type.')
 
                 try:
                     json_obj = request.get_json()
                     if json_obj is None:
                         json_obj = {}
-                    model = query(**json_obj)
+                    model = query(**json_obj) if query else {}
                 except ValidationError as err:
                     abort(422, err)
                 except Exception:
@@ -177,15 +193,27 @@ class Flaskerk:
 
                 request.query = model
                 response = func(*args, **kwargs)
-                if not isinstance(response, resp):
+                if resp and not isinstance(response, resp):
                     abort(500, 'Wrong response type produced by server.')
 
-                return jsonify(**response.dict())
+                return jsonify(**response.dict()) if resp else response
 
-            self.models[query.__name__] = query.schema()
-            self.models[resp.__name__] = resp.schema()
-            validate_request.query = query.__name__
-            validate_request.resp = resp.__name__
+            if query:
+                self.models[query.__name__] = query.schema()
+                validate_request.query = query.__name__
+            if resp:
+                self.models[resp.__name__] = resp.schema()
+                validate_request.resp = resp.__name__
+
+            code_msg = {}
+            for e in expt:
+                if not isinstance(e, HTTPException):
+                    abort(500, 'Wrong exception instance.')
+                code_msg[e.code] = e.msg
+
+            if code_msg:
+                validate_request.expt = code_msg
+
             return validate_request
         return decorate_validate_request
 
--- flaskerk/config.py
+++ flaskerk/config.py
@@ -1,13 +1,34 @@
+from enum import Enum
+
+
+class UI(Enum):
+    """
+    :param: 'redoc' or 'swagger'
+    """
+    redoc = 'redoc'
+    swagger = 'swagger'
+
+
 class Config:
+    """
+    Flaskerk configuration
+    """
     name = 'docs'
+    #: url path for docs
     endpoint = '/docs/'
     url_prefix = None
     template_folder = 'templates'
+    #: openapi spec file name
     filename = 'openapi.json'
 
+    #: openapi spec version
     openapi_veresion = '3.0.2'
+    #: document title
     title = 'Service Documents'
+    #: service version
     version = '0.1'
+    #: :class:`.UI`
+    ui = UI('redoc')
 
 
 default_config = Config()
