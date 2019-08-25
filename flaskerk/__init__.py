1ab953669f8bf0fcd68e81d15b4f20484074827b
keming <kemingy94@gmail.com>
2019-08-25 18:46:22 +0800

    fix all, update example in readme

--- flaskerk/__init__.py
+++ flaskerk/__init__.py
@@ -1,9 +1,11 @@
 from flaskerk.base import Flaskerk
 from flaskerk.exception import HTTPException
+from flaskerk.utils import abort_json
 
 __version__ = '0.2.0'
 
 __all__ = [
     Flaskerk,
     HTTPException,
+    abort_json,
 ]
