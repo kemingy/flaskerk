104d96af9c45ebc1e37a2bd79a7eaa114c469bfb
keming <kemingy94@gmail.com>
2019-09-26 10:37:25 +0800

    add blueprint example

---
+++ examples/blueprint.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, request, Flask, jsonify
+from flaskerk import Flaskerk
+from pydantic import BaseModel
+
+
+class Query(BaseModel):
+    name: str
+    uid: str
+    limit: int = 5
+
+
+bp = Blueprint('bp', __name__)
+api = Flaskerk()
+
+
+@bp.route('/api/recommend', methods=['POST'])
+@api.validate(query=Query)
+def recommend():
+    query = request.query
+    print(query.name, query.uid)
+    return jsonify(users=['xxx'] * query.limit)
+
+
+if __name__ == '__main__':
+    app = Flask(__name__)
+    app.register_blueprint(bp)
+    api.register(app)
+
+    app.run()
