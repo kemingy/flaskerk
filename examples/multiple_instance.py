7d0f65dd6ca7fae75e1ef28b4b892a3ad6fd7d33
keming <kemingy94@gmail.com>
2019-10-10 21:09:15 +0800

    add demo for multiple flaskerk instance

---
+++ examples/multiple_instance.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+from flaskerk import Flaskerk
+from pydantic import BaseModel
+
+
+class Query(BaseModel):
+    text: str
+
+
+app = Flask(__name__)
+api = Flaskerk()
+another = Flaskerk(ui='swagger')
+
+
+@app.route('/api/classify')
+@api.validate(query=Query)
+def classify():
+    print(request.query)
+    return jsonify(label=0)
+
+
+@app.route('/ping')
+def ping():
+    return jsonify(msg='pong')
+
+
+@app.route('/api/recommend')
+@another.validate()
+def recommend():
+    return jsonify(result=[])
+
+
+if __name__ == "__main__":
+    # api.register(app)
+    another.register(app)
+    app.run()
