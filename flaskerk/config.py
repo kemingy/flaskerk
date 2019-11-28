daa247e9650548f29e9d9e3e688e4bab5cf028d4
keming <kemingy94@gmail.com>
2019-11-28 11:37:16 +0800

    add mode to config

--- flaskerk/config.py
+++ flaskerk/config.py
@@ -1,24 +1,30 @@
 class Config:
+    """
+    :ivar endpoint: url path for docs
+    :ivar filename: openapi spec file name
+    :ivar openapi_version: openapi spec version
+    :ivar title: document title
+    :ivar version: service version
+    :ivar ui: ui theme, choose 'redoc' or 'swagger'
+    :ivar mode: mode for route. **normal** includes undecorated routes and
+        routes decorated by this instance. **strict** only includes routes
+        decorated by this instance. **greedy** includes all the routes.
+
+    Flaskerk configuration.
+    """
+
     def __init__(self):
-        """
-        Flaskerk configuration
-        """
         self.name = 'docs'
-        #: url path for docs
         self.endpoint = '/docs/'
         self.url_prefix = None
         self.template_folder = 'templates'
-        #: openapi spec file name
         self.filename = 'openapi.json'
+        self.mode = 'normal'
 
-        #: openapi spec version
         self.openapi_veresion = '3.0.2'
-        #: document title
         self.title = 'Service Documents'
-        #: service version
         self.version = 'latest'
-        #: 'redoc' or 'swagger'
         self.ui = 'redoc'
 
-        #: private
         self._support_ui = {'redoc', 'swagger'}
+        self._support_mode = {'normal', 'greedy', 'strict'}
