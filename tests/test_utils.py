bffd5cb5d670fe2ba3024f18f3dee5f7ae946e67
keming <kemingy94@gmail.com>
2019-12-02 17:59:46 +0800

    refactor converters

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,7 @@
 import pytest
 
-from flaskerk.const import CONVERTER_TYPE_ANY, CONVERTER_TYPE_INT, CONVERTER_TYPE_FLOAT, \
-    CONVERTER_TYPE_UUID, CONVERTER_TYPE_PATH, CONVERTER_TYPE_STRING, CONVERTER_TYPE_DEFAULT
+from flaskerk.converters import CONVERTER_ANY, CONVERTER_INT, CONVERTER_FLOAT, \
+    CONVERTER_UUID, CONVERTER_PATH, CONVERTER_STRING, CONVERTER_DEFAULT
 from flaskerk.utils import parse_url
 
 TEST_PARAM_NAME = 'test_param'
@@ -9,7 +9,7 @@ EXPECTED_PATH = '/{%s}' % TEST_PARAM_NAME
 
 PARSE_URL_PARAMETRIZE = [
     (
-        f'/<{CONVERTER_TYPE_ANY}(test1, test2, test3, test4, "test,test"):{TEST_PARAM_NAME}>',
+        f'/<{CONVERTER_ANY}(test1, test2, test3, test4, "test,test"):{TEST_PARAM_NAME}>',
         (EXPECTED_PATH,
          [{'name': 'test_param', 'in': 'path', 'required': True, 'schema': {
              'type': 'array',
@@ -18,57 +18,57 @@ PARSE_URL_PARAMETRIZE = [
          }}])
     ),
     (
-        f'/<{CONVERTER_TYPE_INT}:{TEST_PARAM_NAME}>',
+        f'/<{CONVERTER_INT}:{TEST_PARAM_NAME}>',
         (EXPECTED_PATH,
          [{'name': 'test_param', 'in': 'path', 'required': True,
            'schema': {'type': 'integer', 'format': 'int32'}}]
          )
     ),
     (
-        f'/<{CONVERTER_TYPE_INT}(max=2):{TEST_PARAM_NAME}>',
+        f'/<{CONVERTER_INT}(max=2):{TEST_PARAM_NAME}>',
         (EXPECTED_PATH,
          [{'name': 'test_param', 'in': 'path', 'required': True,
            'schema': {'type': 'integer', 'maximum': 2, 'format': 'int32'}}]
          )
     ),
     (
-        f'/<{CONVERTER_TYPE_INT}(min=2):{TEST_PARAM_NAME}>',
+        f'/<{CONVERTER_INT}(min=2):{TEST_PARAM_NAME}>',
         (EXPECTED_PATH,
          [{'name': 'test_param', 'in': 'path', 'required': True,
            'schema': {'type': 'integer', 'minimum': 2, 'format': 'int32'}}]
          )
     ),
     (
-        f'/<{CONVERTER_TYPE_FLOAT}:{TEST_PARAM_NAME}>',
+        f'/<{CONVERTER_FLOAT}:{TEST_PARAM_NAME}>',
         (EXPECTED_PATH,
          [{'name': 'test_param', 'in': 'path', 'required': True, 'schema': {
              'type': 'number', 'format': 'float'}}])
     ),
     (
-        f'/<{CONVERTER_TYPE_UUID}:{TEST_PARAM_NAME}>',
+        f'/<{CONVERTER_UUID}:{TEST_PARAM_NAME}>',
         (EXPECTED_PATH,
          [{'name': 'test_param', 'in': 'path', 'required': True, 'schema': {
              'type': 'string', 'format': 'uuid'}}])
     ),
     (
-        f'/<{CONVERTER_TYPE_PATH}:{TEST_PARAM_NAME}>',
+        f'/<{CONVERTER_PATH}:{TEST_PARAM_NAME}>',
         (EXPECTED_PATH,
          [{'name': 'test_param', 'in': 'path', 'required': True, 'schema': {
              'type': 'string', 'format': 'path'}}])
     ),
     (
-        f'/<{CONVERTER_TYPE_STRING}:{TEST_PARAM_NAME}>',
+        f'/<{CONVERTER_STRING}:{TEST_PARAM_NAME}>',
         (EXPECTED_PATH,
          [{'name': 'test_param', 'in': 'path', 'required': True, 'schema': {'type': 'string'}}])
     ),
     (
-        f'/<{CONVERTER_TYPE_STRING}(length=2):{TEST_PARAM_NAME}>',
+        f'/<{CONVERTER_STRING}(length=2):{TEST_PARAM_NAME}>',
         (EXPECTED_PATH,
          [{'name': 'test_param', 'in': 'path', 'required': True, 'schema': {
              'length': 2, 'type': 'string'}}])
     ),
     (
-        f'/<{CONVERTER_TYPE_DEFAULT}:{TEST_PARAM_NAME}>',
+        f'/<{CONVERTER_DEFAULT}:{TEST_PARAM_NAME}>',
         (EXPECTED_PATH,
          [{'name': 'test_param', 'in': 'path', 'required': True, 'schema': {'type': 'string'}}])
     ),
